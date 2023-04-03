bd3b414cfd8db82d29c8d5f0ee85d33c71b01547
mdtarifulislam <rupkothargolpo830@gmail.com>
2023-04-04 01:15:51 +0600

    first comm

---
+++ index.js
@@ -0,0 +1,7 @@
+
+var animation = document.querySelector(".btn");
+animation.addEventListener("click", function () {
+    document.querySelector(".motion_file").style.visibility = "visible";
+    document.querySelector(".btn").style.visibility = "hidden";
+
+})
